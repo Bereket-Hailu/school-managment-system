bc702b098604da8eb8a72f6421c71e2ce2cd599f
Mohamed Amano <mohamish1995@gmail.com>
2024-06-01 03:15:38 +0900

    Update a_logout.php

--- admin/a_logout.php
+++ admin/a_logout.php
@@ -44,3 +44,64 @@
 
 
 <?php
+
+include('../dbcon.php');
+
+
+if(isset($_POST['alogin'])){
+    
+    
+    $a_username= $_POST['auser'];
+    $a_pass= $_POST['apass'];
+    ?>
+
+        <script>
+            alert('username or password '); 
+        </script>
+    <?php
+
+    
+    $query= "SELECT * FROM `admin` WHERE  `user`='$a_username' AND `pass`='$a_pass' ";
+    
+    $res=mysqli_query($con,$query);
+
+    $row = mysqli_num_rows($res);
+    
+
+
+    if($row < 1){
+       echo "running";
+        ?>
+        <script>
+            alert('username or password does not match!');
+            window.open('a_login.php','_self');
+        </script>
+        <!--window.open('../student/a_login.php','_self');  -->
+        <?php
+        //header('Location:student/a_login.php');
+    }
+    else{
+        
+        $data = mysqli_fetch_assoc($res);
+
+        $current_id= $data['id'];
+        $current_user= $data['user'];
+
+        echo "id :" .$current_id;
+        echo "name: ".$current_user;
+
+        ?>
+        <script>
+            alert('username or password match!');
+        </script>
+        <?php
+        $_SESSION['aid']=$current_id;   //new variable sid 
+        $_SESSION['auser']=$current_user; 
+
+        header('location:../admin/dashboard.php');
+
+    }
+}
+
+
+?>
